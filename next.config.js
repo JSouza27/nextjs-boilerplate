8bfaa31fac2d1daf37274e6e329e581c361d7ebc
Jonathan <souzajonathan570@gmail.com>
2023-01-09 19:10:58 -0300

    fix: fix jest styled components

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
 const nextConfig = {
   reactStrictMode: true
 }
 
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
+module.exports = nextConfig
